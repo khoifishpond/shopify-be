22cdb4a5c9f759466f6da50ccdbccefc85cc806e
Khoi Nguyen <khoinguyen311@gmail.com>
2022-05-22 00:01:48 +0000

    add items and warehouses resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "welcome#index"
+  resources :items
+  resources :warehouses, except: [:destroy]
 end
